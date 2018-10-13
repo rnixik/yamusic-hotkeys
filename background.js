78a93868e7026d26c50580b9a71e7b406383a687
rNix <rnix@yandex.ru>
2018-10-13 17:14:36 +0700

    Fix sending to closed port

--- background.js
+++ background.js
@@ -3,18 +3,25 @@
 
     const PORT_NAME = 'music-hotkeys-port';
 
-    chrome.runtime.onInstalled.addListener(function () {
+    chrome.runtime.onInstalled.addListener(() => {
 
         let messagePort;
 
-        chrome.runtime.onConnect.addListener(function(port) {
+        chrome.runtime.onConnect.addListener((port) => {
             if (port.name !== PORT_NAME) {
                 return;
             }
             messagePort = port;
+
+            messagePort.onDisconnect.addListener(() => {
+                messagePort = null;
+            });
         });
 
-        chrome.commands.onCommand.addListener(function (command) {
+        chrome.commands.onCommand.addListener((command) => {
+            if (!messagePort) {
+                return;
+            }
             messagePort.postMessage({ command });
         });
     });
