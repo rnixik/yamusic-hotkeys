bca50a9190cd660690f395508e7c594da65ac872
rNix <rnix@yandex.ru>
2018-10-13 20:45:23 +0700

    Move binding for events to root of background script

--- background.js
+++ background.js
@@ -3,29 +3,28 @@
 
     const PORT_NAME = 'music-hotkeys-port';
 
-    chrome.runtime.onInstalled.addListener(() => {
+    let messagePort;
 
-        let messagePort;
+    console.log("Prepare hot keys");
 
-        chrome.runtime.onConnect.addListener((port) => {
-            if (port.name !== PORT_NAME) {
-                return;
-            }
-            messagePort = port;
-            console.log("Connected to port");
+    chrome.runtime.onConnect.addListener((port) => {
+        if (port.name !== PORT_NAME) {
+            return;
+        }
+        messagePort = port;
+        console.log("Connected to port");
 
-            messagePort.onDisconnect.addListener(() => {
-                messagePort = null;
-                console.log("Disconnected to port");
-            });
+        messagePort.onDisconnect.addListener(() => {
+            messagePort = null;
+            console.log("Disconnected to port");
         });
+    });
 
-        chrome.commands.onCommand.addListener((command) => {
-            console.log("Command", command);
-            if (!messagePort) {
-                return;
-            }
-            messagePort.postMessage({ command });
-        });
+    chrome.commands.onCommand.addListener((command) => {
+        console.log("Command", command);
+        if (!messagePort) {
+            return;
+        }
+        messagePort.postMessage({ command });
     });
 })();
